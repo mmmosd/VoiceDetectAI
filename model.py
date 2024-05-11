67379633877d97790d0893f6db4c4080a03d3f49
mmmosd-games <chlgustj132@gmail.com>
2024-05-11 11:06:30 +0900

    Update model.py

---
+++ model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+EPOCH = 100
+BATCH_SIZE = 32
+
+
+class model(nn.Module):
+    def __init__(self):
+        super(model, self).__init__()
+    def forward(self, x):
+        return x
+    
+# def train():
+    
